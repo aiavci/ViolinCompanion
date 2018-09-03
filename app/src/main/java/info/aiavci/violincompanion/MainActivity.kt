ac800b241799d2d3b36008e1e3ae1ee9c949211a
Ali Avci <aiavci@yahoo.com>
2018-09-02 21:56:55 -0400

    first commit

---
+++ app/src/main/java/info/aiavci/violincompanion/MainActivity.kt
@@ -0,0 +1,13 @@
+package info.aiavci.violincompanion
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import info.aiavci.violincompanion.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

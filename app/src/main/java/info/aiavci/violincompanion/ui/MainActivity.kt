0a43d6f47c4dcd0f1ee0cab25344a832f1449b73
Ali Avci <aiavci@yahoo.com>
2018-09-04 20:46:55 -0400

    Add back button

--- app/src/main/java/info/aiavci/violincompanion/ui/MainActivity.kt
+++ app/src/main/java/info/aiavci/violincompanion/ui/MainActivity.kt
@@ -2,7 +2,6 @@ package info.aiavci.violincompanion.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-
 import info.aiavci.violincompanion.R
 import kotlinx.android.synthetic.main.activity_main.*
 
--- app/src/main/java/info/aiavci/violincompanion/ui/ScaleActivity.kt
+++ app/src/main/java/info/aiavci/violincompanion/ui/ScaleActivity.kt
@@ -1,9 +1,21 @@
 package info.aiavci.violincompanion.ui
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 /**
  * Created by ${$FULLNAME}
  * Version
  */
-class ScaleActivity: AppCompatActivity()
+class ScaleActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return super.onSupportNavigateUp()
+    }
+}
